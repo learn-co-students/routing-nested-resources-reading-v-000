bf60d45dc473a4aeffac542ddcba31a6befb439d
selichter <lichter.sarahe@gmail.com>
2016-02-03 14:50:47 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  get 'authors/:id/posts', to: 'authors#posts_index'
+
+  get 'authors/:id/posts/:post_id', to: 'authors#post'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
