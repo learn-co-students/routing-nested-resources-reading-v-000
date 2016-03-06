0d63ea74797ed6c8923265a7d1af8dfefff7641f
Chris Aporta <christopher.aporta@gmail.com>
2016-03-06 18:53:46 -0500

    complete lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  #get 'authors/:id/posts', to: 'authors#post_index'
+  #get 'authors/:id/posts/:post_id', to: 'authors#post'
+
 end
