4a220ea81f79491b68e19914a1a7ebc3c9f1fa10
bacitracin <tracetrace@gmail.com>
2016-04-11 01:34:47 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,21 @@
 class PostsController < ApplicationController
 
-  def index
-    @posts = Post.all
+   def index
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
   end
-
+ 
   def show
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      @post = Author.find(params[:author_id]).posts.find(params[:id])
+    else
+      @post = Post.find(params[:id])
+    end
   end
-
+ 
   def new
     @post = Post.new
   end
