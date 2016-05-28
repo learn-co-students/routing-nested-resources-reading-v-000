90d3d8f1ec3f970c001114923d4a9c654e2928fb
Joshua Azemoh <joshazemoh@gmail.com>
2016-05-28 19:35:58 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,19 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if params[:author_id]
+      @posts = Post.where(author_id: params[:author_id])
+    else
+      @posts = Post.all
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      @post = Post.find_by(id: params[:id], author_id: params[:author_id])
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   def new
