834fb8931da005bd9d65b0dd78a5730c3faaf8e2
Heber Sandoval <he3ers@gmail.com>
2016-06-02 06:01:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :authors, only: [:show] do
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
 
   resources :authors, only: [:show]
 
