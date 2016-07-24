ad59626692c964e39a7ddb80721e0969b5e4aefa
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-24 02:02:13 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
 
   def index
+  if params[:author_id]
+    @posts = Author.find(params[:author_id]).posts
+  else
     @posts = Post.all
   end
+end
 
-  def show
+def show
+  if params[:author_id]
+    @post = Post.find(params[:id])
+  else
     @post = Post.find(params[:id])
   end
+end
 
   def new
     @post = Post.new
