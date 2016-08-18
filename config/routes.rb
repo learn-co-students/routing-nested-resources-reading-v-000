82cd39f444f417ecd5d400006f1b6a8c8938aff8
Christian Whitesides <whitesidescr@gmail.com>
2016-08-18 17:21:38 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  # Instead of this...
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/post/:post_id', to: 'authors#post'
+
+  # Do this...
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
