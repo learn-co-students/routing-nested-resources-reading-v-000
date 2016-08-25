f3f5917a0ee6c61df2664cccc5fd6e22706b7412
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-08-24 21:09:31 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors do
+  	resources :posts do
+  		resources :comments
+  	end
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+
 end
