21f403f936e2dfe07d78355ff7adad26fc45f393
karuna24s <karuna24s@gmail.com>
2016-09-01 06:14:21 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,11 @@
 class AuthorsController < ApplicationController
+  before_action :find_author, only: [:show]
 
   def show
+  end
+
+  private
+  def find_author
     @author = Author.find(params[:id])
   end
 
