0b1f673e1a25db386734c3042e11a548764d1861
nichia <nichia@gmail.com>
2018-10-27 23:09:25 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/posts/:posts_id', to: 'authors#post'
 
   root 'posts#index'
 end
