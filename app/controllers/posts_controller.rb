865c427aa95ebc0e455785f14a87785230cfc612
btmccollum <btmcco8@gmail.com>
2018-11-13 12:58:26 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
   end
-
+ 
   def show
     @post = Post.find(params[:id])
   end
@@ -27,6 +31,7 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+  
 
 private
 
