522093f723a430ae153559f01b09d4adb1c255b6
Ayana Bando <ayanaflax@yahoo.com>
2019-02-13 02:35:01 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,10 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
-
+  
+  def post
+    @author = Author.find(params[:id])
+    @post = Post.find(params[:post_id])
+    render template: 'posts/show'
+  end
 end
