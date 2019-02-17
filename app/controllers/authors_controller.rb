5f1faceba82ff8b6f82de4a2563469d1e02107a2
github username <github email address>
2019-02-17 13:29:47 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,7 @@
 class AuthorsController < ApplicationController
 
-  def posts_index
-     @author = Author.find(params[:id])
-     @posts = @author.posts
-     render template: 'posts/index'
-   end
-
-   def post
-     @author = Author.find(params[:id])
-
-     @post = Post.find(params[:post_id])
-     render template: 'posts/show'
-   end
+  def show
+   @author = Author.find(params[:id])
+ end
 
 end
