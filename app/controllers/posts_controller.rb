b8cc5c9f270fd78cb2d8bdccc69bc3f5573053af
AlwinaO <aoyewole2586@gmail.com>
2019-05-05 20:56:58 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).post_status
+    else
+      @posts = Post.all
+    end
   end
 
   def show
