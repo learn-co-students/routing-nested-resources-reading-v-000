442cf786bf7464db03412da6cf43e4bfbadd8ba4
github username <github email address>
2019-09-11 23:21:36 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,24 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
+  #We know we can build out a route with dynamic segments, so our first instinct might be to just define these in routes.rb like this:
+
+  #get 'authors/:id/posts'
+  #get 'authors/:id/posts/:post_id'
+
+  #Gotta tell those routes explicitly which controller actions will handle them. Okay, let's make it look more like this:
+
+  #get 'authors/:id/posts', to: 'authors#posts_index'
+  #get 'authors/:id/posts/:post_id', to: 'authors#post'
+
+  #Nested resources give us a way to document that parent/child relationship in our routes and, ultimately, our URLs.
+
+  resources :authors, only: [:show] do
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
+ 
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+ 
+  root 'posts#index'
 end
